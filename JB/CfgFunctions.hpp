e740c53ff9c2cb45dbb1603273c2a2a9ef2a94d4
David Citarelli <davidcit646@gmail.com>
2019-12-29 01:48:54 -0500

    Vehicle Respawn Stuff

--- JB/CfgFunctions.hpp
+++ JB/CfgFunctions.hpp
@@ -2,7 +2,13 @@ class JB {
 	tag = "JB";
 	class functions {
 		file = "JB";
+		//greenzone related (to the best of my understanding)
 		class displayName {};
 		class showBlackScreenMessage{};
+
+		//vehicle respawns
+		respawnVehicleWhenAbandoned
+		respawnVehicleWhenKilled
+		respawnVehicleInitialize
 	};
 };
